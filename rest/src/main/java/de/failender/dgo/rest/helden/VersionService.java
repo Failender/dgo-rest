2266f4b5c7d49ead9f0aa93a0f4204754554fb5b
Patrick Hilgenstock <pahilgenstock@hotmail.de>
2020-01-01 12:10:58 +0100

    Implement comparing versions

--- rest/src/main/java/de/failender/dgo/rest/helden/VersionService.java
+++ rest/src/main/java/de/failender/dgo/rest/helden/VersionService.java
@@ -7,22 +7,27 @@ import de.failender.dgo.persistance.held.VersionRepositoryService;
 import de.failender.dgo.persistance.user.UserEntity;
 import de.failender.dgo.persistance.user.UserRepositoryService;
 import de.failender.dgo.rest.integration.Beans;
+import de.failender.heldensoftware.Unterscheidbar;
 import de.failender.heldensoftware.api.HeldenApi;
 import de.failender.heldensoftware.api.XmlUtil;
 import de.failender.heldensoftware.api.authentication.TokenAuthentication;
 import de.failender.heldensoftware.api.requests.*;
 import de.failender.heldensoftware.xml.datenxml.Daten;
+import de.failender.heldensoftware.xml.datenxml.Eigenschaftswerte;
 import de.failender.heldensoftware.xml.datenxml.Ereignis;
+import one.util.streamex.EntryStream;
 import reactor.core.publisher.Mono;
 import reactor.util.function.Tuple2;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class VersionService {
+
     public static VersionEntity persistVersion(HeldEntity held, int version, String xml, UUID uuid, Daten daten) {
         LocalDateTime date = XmlUtil.getStandFromString(xml);
         VersionEntity versionEntity = new VersionEntity();
@@ -129,5 +134,101 @@ public class VersionService {
             }
         }
         VersionRepositoryService.deleteVersions(versionEntities);
+    }
+
+    public static Differences calculateDifferences(Long heldenid, int from, int to) {
+        //If from is bigger then to flip the values
+        if (from > to) {
+            int tempFrom = from;
+            from = to;
+            to = tempFrom;
+        }
+        HeldEntity held = HeldRepositoryService.findById(heldenid);
+        VersionEntity fromVersion = VersionRepositoryService.findVersion(held, from);
+        VersionEntity toVersion = VersionRepositoryService.findVersion(held, to);
+        return calculateDifferences(held, fromVersion, toVersion);
+    }
+
+    private static Differences calculateDifferences(HeldEntity held, VersionEntity from, VersionEntity to) {
+        Tuple2<Daten, Daten> datenTuple = Beans.HELDEN_API.request(new ReturnHeldDatenWithEreignisseRequest(held.getId(), null, from.getCacheId()))
+                .zipWith(Beans.HELDEN_API.request(new ReturnHeldDatenWithEreignisseRequest(held.getId(), null, to.getCacheId()))).block();
+        return calculateDifferences(held.getName(), datenTuple.getT1(), datenTuple.getT2());
+    }
+
+    private static Differences calculateDifferences(String name, Daten from, Daten to) {
+        //List of all ereignisse that are not in from but in to
+        List<Ereignis> ereignisList = to.getEreignisse().getEreignis().subList(from.getEreignisse().getEreignis().size(), to.getEreignisse().getEreignis().size());
+        Map<String, List<Ereignis>> map = EntryStream.of(ereignisList)
+                .mapToKey((idx, ereignis) ->
+                        ereignis.getObject()
+                                .replaceFirst("Sprachen kennen ", "")
+                                .replaceFirst("Lesen/Schreiben", "L/S")
+                                .replaceFirst("Ritualkenntnis: ", "")
+                                .replaceAll(" \\[.*\\]", "")).grouping();
+        return new Differences(name, calculateDifference(from.getTalentliste().getTalent(), to.getTalentliste().getTalent(), map, true),
+                calculateDifference(from.getZauberliste().getZauber(), to.getZauberliste().getZauber(), map, true),
+                calculateDifference(from.getVorteile().getVorteil(), to.getVorteile().getVorteil(), map, false),
+                calculateEigenschaftDifference(from, to, map),
+                calculateSonderfertigkeitenDifference(ereignisList));
+    }
+
+    private static List<Difference> calculateEigenschaftDifference(Daten from, Daten to, Map<String, List<Ereignis>> ereignis) {
+        return Stream.of(calculateEigenschaftDifference(from.getEigenschaften().getMut(), to.getEigenschaften().getMut(), ereignis), calculateEigenschaftDifference(from.getEigenschaften().getKlugheit(), to.getEigenschaften().getKlugheit(), ereignis), calculateEigenschaftDifference(from.getEigenschaften().getIntuition(), to.getEigenschaften().getIntuition(), ereignis), calculateEigenschaftDifference(from.getEigenschaften().getCharisma(), to.getEigenschaften().getCharisma(), ereignis), calculateEigenschaftDifference(from.getEigenschaften().getFingerfertigkeit(), to.getEigenschaften().getFingerfertigkeit(), ereignis), calculateEigenschaftDifference(from.getEigenschaften().getGewandtheit(), to.getEigenschaften().getGewandtheit(), ereignis), calculateEigenschaftDifference(from.getEigenschaften().getKonstitution(), to.getEigenschaften().getKonstitution(), ereignis), calculateEigenschaftDifference(from.getEigenschaften().getKoerperkraft(), to.getEigenschaften().getKoerperkraft(), ereignis)).filter(Objects::nonNull).collect(Collectors.toList());
+    }
+
+    private static List<Difference> calculateSonderfertigkeitenDifference(List<Ereignis> ereignis) {
+        List<Difference> differences = new ArrayList<>();
+        for (Ereignis ereigni : ereignis) {
+            if (ereigni.getAktion().startsWith("Sonderfertigkeit")) {
+                differences.add(new Difference(-ereigni.getAp(), "aktiv", ereigni.getObject(), ereigni.getKommentar()));
+            }
+        }
+        return differences;
+    }
+
+    private static Difference calculateEigenschaftDifference(Eigenschaftswerte from, Eigenschaftswerte to, Map<String, List<Ereignis>> map) {
+        if (from.getAkt().equals(to.getAkt())) {
+            return null;
+        }
+        String name = from.getName();
+        int fromVal = from.getAkt().intValue();
+        int toVal = to.getAkt().intValue();
+        String tooltip = calculateTooltip(map, name, false);
+        return new Difference(fromVal, toVal, name, tooltip);
+    }
+
+    private static List<Difference> calculateDifference(List<? extends Unterscheidbar> fromList, List<? extends Unterscheidbar> toList, Map<String, List<Ereignis>> ereignisse, boolean replaceSpace) {
+        List<Difference> differences = new ArrayList<>();
+        fromList.forEach(from -> {
+            Optional<? extends Unterscheidbar> toOptional = toList.stream().filter(to -> to.name().equals(from.name())).findFirst();
+            if (toOptional.isPresent()) {
+                Unterscheidbar to = toOptional.get();
+                toList.remove(to);
+                Integer fromWert = from.getWert();
+                Integer toWert = to.getWert();
+                if (fromWert == null && toWert == null || fromWert != null && toWert != null && fromWert.equals(toWert)) {
+                    return;
+                }
+                differences.add(new Difference(fromWert, toWert, from.name(), ""));
+            } else {
+                differences.add(new Difference(from.getWert(), null, from.name(), ""));
+            }
+        });
+        toList.forEach(toTalent -> differences.add(new Difference(null, toTalent.getWert(), toTalent.name(), "")));
+        differences.forEach(difference -> {
+            String tooltip = calculateTooltip(ereignisse, difference.getName(), replaceSpace);
+            difference.setTooltip(tooltip);
+        });
+        return differences;
+    }
+
+    private static String calculateTooltip(Map<String, List<Ereignis>> map, String name, boolean replaceSpace) {
+        return map.getOrDefault(name, Collections.emptyList()).stream().map(ereignis -> ereignis.getBemerkung().replace(",", "")).map(value -> {
+            if (replaceSpace && value.isEmpty()) {
+                return "Aktiviert";
+            } else {
+                return value;
+            }
+        }).collect(Collectors.joining(", "));
     }
 }
--- rest/src/main/java/de/failender/dgo/rest/helden/version/VersionController.java
+++ rest/src/main/java/de/failender/dgo/rest/helden/version/VersionController.java
@@ -3,7 +3,9 @@ package de.failender.dgo.rest.helden.version;
 import de.failender.dgo.persistance.held.HeldEntity;
 import de.failender.dgo.persistance.held.VersionEntity;
 import de.failender.dgo.persistance.held.VersionRepositoryService;
+import de.failender.dgo.rest.helden.Differences;
 import de.failender.dgo.rest.helden.HeldenService;
+import de.failender.dgo.rest.helden.VersionService;
 import io.javalin.Context;
 import io.javalin.Javalin;
 
@@ -13,17 +15,27 @@ import java.util.stream.Collectors;
 public class VersionController {
 
     public static final String PREFIX = "api/helden/versionen/";
-    public static final String FOR_HELD= PREFIX + "held/:held";
+    public static final String FOR_HELD = PREFIX + "held/:held/";
+    public static final String COMPARE = FOR_HELD + "compare/:from/:to";
 
 
     public VersionController(Javalin app) {
         app.get(FOR_HELD, this::getVersionenForHeld);
+        app.get(COMPARE, this::compare);
     }
 
     private void getVersionenForHeld(Context ctx) {
         HeldEntity heldEntity = HeldenService.getHeldEntity(ctx);
         List<VersionEntity> versionEntityList =VersionRepositoryService.findVersionsByHeld(heldEntity);
         ctx.json(versionEntityList.stream().map(VersionDto::new).collect(Collectors.toList()));
+    }
+
+    private void compare(Context ctx) {
+        Long id = Long.valueOf(ctx.pathParam("held"));
+        int from = Integer.valueOf(ctx.pathParam("from"));
+        int to = Integer.valueOf(ctx.pathParam("to"));
+        Differences differences = VersionService.calculateDifferences(id, from, to);
+        ctx.json(differences);
 
 
     }
